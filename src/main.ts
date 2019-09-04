f2c8dfd2ae817296de34b0d810c952616c996423
Tim Gymnich <TG908@users.noreply.github.com>
2019-09-05 01:11:14 +0200

    inital commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import * as core from '@actions/core';
+const github = require('@actions/github');
+
+const myToken = core.getInput('github-token');
+const context = github.context;
+const octokit = new github.GitHub(myToken);
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const owner = core.getInput('owner');
+    const destinationBranch = core.getInput('destinationBranch');
+    const originBranch = core.getInput('originBranch');
+    const mergeMethod = core.getInput('mergeMethod');
+
+    octokit.pulls.create({owner: context.repo.owner, repo: context.repo.repo, title: 'Sync Fork', head: owner+':'+originBranch, base: destinationBranch, merge_method: mergeMethod})
+    .then(pr => octokit.pulls.merge({owner: pr.data.user.login, repo: context.repo.repo, pull_number: pr.data.number}));
+
   } catch (error) {
     core.setFailed(error.message);
   }
