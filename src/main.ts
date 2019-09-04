e964a2cfb4d4106e4a1e3b0351717b2bb47c699f
Tim Gymnich <TG908@users.noreply.github.com>
2019-09-05 01:24:24 +0200

    added github_token

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import * as core from '@actions/core';
 const github = require('@actions/github');
 
-const myToken = core.getInput('github-token');
+const githubToken = core.getInput('github_token');
 const context = github.context;
-const octokit = new github.GitHub(myToken);
+const octokit = new github.GitHub(githubToken);
 
 async function run() {
   try {
     const owner = core.getInput('owner');
-    const destinationBranch = core.getInput('destinationBranch');
-    const originBranch = core.getInput('originBranch');
-    const mergeMethod = core.getInput('mergeMethod');
+    const destinationBranch = core.getInput('destination_branch');
+    const originBranch = core.getInput('origin_branch');
+    const mergeMethod = core.getInput('merge_method');
 
     octokit.pulls.create({owner: context.repo.owner, repo: context.repo.repo, title: 'Sync Fork', head: owner+':'+originBranch, base: destinationBranch, merge_method: mergeMethod})
     .then(pr => octokit.pulls.merge({owner: pr.data.user.login, repo: context.repo.repo, pull_number: pr.data.number}));
