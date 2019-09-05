4a5297ab601b3b8f068624da01b5385d1fef349c
Tim Gymnich <TG908@users.noreply.github.com>
2019-09-05 03:56:33 +0200

    Added checks

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,29 @@
 import * as core from '@actions/core';
 const github = require('@actions/github');
 
-const githubToken = core.getInput('github_token');
+const githubToken = core.getInput('github_token', {required: true});
 const context = github.context;
 const octokit = new github.GitHub(githubToken);
 
 async function run() {
   try {
-    const owner = core.getInput('owner') || context.repo.owner;
-    const destinationBranch = core.getInput('destination_branch');
-    const originBranch = core.getInput('origin_branch');
-    const mergeMethod = core.getInput('merge_method');
-    const prTitle = core.getInput('pr_title');
-    const prMessage = core.getInput('pr_message');
+    const owner = core.getInput('owner', {required: false}) || context.repo.owner;
+    const destinationBranch = core.getInput('destination_branch', {required: false});
+    const originBranch = core.getInput('origin_branch', {required: false});
+    const mergeMethod = core.getInput('merge_method', {required: false});
+    const prTitle = core.getInput('pr_title', {required: false});
+    const prMessage = core.getInput('pr_message', {required: false});
 
     await octokit.pulls.create({owner: context.repo.owner, repo: context.repo.repo, title: prTitle, head: owner+':'+originBranch, base: destinationBranch, body: prMessage, merge_method: mergeMethod})
       .then((pr) => {
         octokit.pulls.merge({owner: context.repo.owner, repo: context.repo.repo, pull_number: pr.data.number});
       })
       .catch((err) => {
-        core.setFailed(err.message);
+        core.setFailed('Failed to create or merge PR: '+err.message);
     });
 
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed('Error setting up action: '+error.message);
   }
 }
 
