64d76d9501872dbabf47b706bb07b5605f0e2ee6
Tim Gymnich <TG908@users.noreply.github.com>
2019-09-05 02:15:31 +0200

    Added some more options

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,14 @@ const octokit = new github.GitHub(githubToken);
 
 async function run() {
   try {
-    const owner = core.getInput('owner');
+    const owner = core.getInput('owner') || context.repo.owner;
     const destinationBranch = core.getInput('destination_branch');
     const originBranch = core.getInput('origin_branch');
     const mergeMethod = core.getInput('merge_method');
+    const prTitle = core.getInput('pr_title');
+    const prMessage = core.getInput('pr_message');
 
-    await octokit.pulls.create({owner: context.repo.owner, repo: context.repo.repo, title: 'Sync Fork', head: owner+':'+originBranch, base: destinationBranch, merge_method: mergeMethod})
+    await octokit.pulls.create({owner: context.repo.owner, repo: context.repo.repo, title: prTitle, head: owner+':'+originBranch, base: destinationBranch, body: prMessage, merge_method: mergeMethod})
       .then((pr) => {
         octokit.pulls.merge({owner: context.repo.owner, repo: context.repo.repo, pull_number: pr.data.number});
       })
