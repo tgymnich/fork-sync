ee2964eb3111641d8cc77c6b3f24cb9fd20c64a6
Ashe <tech@ashe.kr>
2020-06-30 00:04:42 +0900

    Add Option IgnoreFail

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function run() {
   const mergeMethod = core.getInput('merge_method', { required: false });
   const prTitle = core.getInput('pr_title', { required: false });
   const prMessage = core.getInput('pr_message', { required: false });
+  const ignoreFail = core.getInput('ignore_fail', { required: false });
 
   try {
     let pr = await octokit.pulls.create({ owner: context.repo.owner, repo: context.repo.repo, title: prTitle, head: owner + ':' + head, base: base, body: prMessage, merge_method: mergeMethod, maintainer_can_modify: false });
@@ -20,8 +21,10 @@ async function run() {
     if (!!error.errors && error.errors[0].message == 'No commits between ' + context.repo.owner + ':' + base + ' and ' + owner + ':' + head) {
       console.log('No commits between ' + context.repo.owner + ':' + base + ' and ' + owner + ':' + head);
     } else {
-      console.log(error)
-      core.setFailed('Failed to create or merge pull request');
+      if (!ignoreFail) {
+        console.log(error);
+        core.setFailed('Failed to create or merge pull request');
+      }
     }
   }
 }
