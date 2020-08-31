09747707834bea8f23bc653600226f24f91b4d2a
Tim Gymnich <tg908@icloud.com>
2020-08-31 16:10:40 +0200

    better error reporting

--- src/main.ts
+++ src/main.ts
@@ -18,12 +18,12 @@ async function run() {
     let pr = await octokit.pulls.create({ owner: context.repo.owner, repo: context.repo.repo, title: prTitle, head: owner + ':' + head, base: base, body: prMessage, merge_method: mergeMethod, maintainer_can_modify: false });
     await octokit.pulls.merge({ owner: context.repo.owner, repo: context.repo.repo, pull_number: pr.data.number });
   } catch (error) {
-    if (!!error.errors && error.errors[0].message == 'No commits between ' + context.repo.owner + ':' + base + ' and ' + owner + ':' + head) {
+    if (error?.errors[0]?.message == 'No commits between ' + context.repo.owner + ':' + base + ' and ' + owner + ':' + head) {
       console.log('No commits between ' + context.repo.owner + ':' + base + ' and ' + owner + ':' + head);
     } else {
       if (!ignoreFail) {
-        console.log(error);
-        core.setFailed('Failed to create or merge pull request');
+        let message = error?.errors[0]?.message;
+        core.setFailed(`Failed to create or merge pull request: ${message ?? "[n/a]"}`);
       }
     }
   }
