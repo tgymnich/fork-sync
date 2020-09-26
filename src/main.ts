4c513e4721935a219e42e1bb372cb61c14c54d4e
Tim Gymnich <tgymnich@icloud.com>
2020-09-26 17:36:10 +0200

    added debug log for retry count

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,11 @@ async function run() {
     let pr = await octokit.pulls.create({ owner: context.repo.owner, repo: context.repo.repo, title: prTitle, head: owner + ':' + head, base: base, body: prMessage, merge_method: mergeMethod, maintainer_can_modify: false });
     await octokit.pulls.merge({ owner: context.repo.owner, repo: context.repo.repo, pull_number: pr.data.number });
   } catch (error) {
+    if (error.request.request.retryCount) {
+      console.log(
+        `request failed after ${error.request.request.retryCount} retries`
+      );
+    }
     if (!!error.errors && error.errors[0].message.startsWith('No commits between')) {
       console.log('No commits between ' + context.repo.owner + ':' + base + ' and ' + owner + ':' + head);
     } else {
